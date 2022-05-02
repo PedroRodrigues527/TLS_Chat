c7e09766814dbd87a9967ecde5ccc2aee0dfabbb
JoaoCupido <jcupido13@gmail.com>
2022-05-02 13:58:54 +0100

    user's other choices (key size and hash)

--- src/main/java/MainClient.java
+++ src/main/java/MainClient.java
@@ -6,8 +6,6 @@ import java.util.Scanner;
 public class MainClient {
 
     public static void main ( String[] args ) throws IOException {
-        Scanner usrInput = new Scanner( System.in );
-
         //Insert username
         String userName = usernameChoice();
 
@@ -15,7 +13,13 @@ public class MainClient {
         String encryptionUser = encryptionChoice();
 
         //Insert key size
-        //String keyUserSize = keySizeChoice();
+        int keyUserSize = keySizeChoice(encryptionUser);
+        System.out.println(keyUserSize);
+
+        //Insert hash mode
+        String hashUser = hashChoice(encryptionUser);
+
+        //System.out.println("User " + userName + " uses " + encryptionUser + " with key size " + keyUserSize + " with hash " + hashUser);
 
         Client client = new Client( "127.0.0.1" , 8000 , userName );
         client.readMessages( );
@@ -57,21 +61,73 @@ public class MainClient {
         return userchoice;
     }
 
-    public static String keySizeChoice()
+    public static int keySizeChoice(String encryption_choice)
     {
         Scanner usrInput = new Scanner( System.in );
 
         //Insert key size choice
         String userchoice;
-        String[] encryptions = {"AES", "DES", "3DES", "RSA"};
+        int key_size_choice = 0;
         do {
-            System.out.println( "Choose type of encryption (AES, DES, 3DES or RSA): ");
-            userchoice = usrInput.nextLine();
-            if ( !Arrays.asList(encryptions).contains(userchoice) )
+            switch (encryption_choice)
             {
-                System.out.println( "ERROR: Unknown choice. Back to Choose Encryption..." );
+                case "AES":
+                    System.out.println( "Choose key size (128, 192 or 256 bits): ");
+                    userchoice = usrInput.nextLine();
+                    String[] keySizes = {"128", "192", "256"};
+                    if( !Arrays.asList(keySizes).contains(userchoice))
+                    {
+                        System.out.println( "ERROR: Unknown choice. Back to Choose Key Size..." );
+                    }
+                    else
+                    {
+                        key_size_choice = Integer.parseInt(userchoice);
+                    }
+                    break;
+                case "DES":
+                    key_size_choice = 56;
+                    break;
+                case "3DES":
+                    key_size_choice = 168;
+                    break;
+                case "RSA":
+                    System.out.println( "Choose key size (1024, 2048 or 4096 bits): ");
+                    userchoice = usrInput.nextLine();
+                    String[] keySizes_rsa = {"1024", "2048", "4096"};
+                    if( !Arrays.asList(keySizes_rsa).contains(userchoice))
+                    {
+                        System.out.println( "ERROR: Unknown choice. Back to Choose Key Size..." );
+                    }
+                    else
+                    {
+                        key_size_choice = Integer.parseInt(userchoice);
+                    }
+                    break;
+                default:
+                    break;
             }
-        } while (!Arrays.asList(encryptions).contains(userchoice));
+        } while (key_size_choice == 0);
+        return key_size_choice;
+    }
+
+    public static String hashChoice(String encryptionChoice)
+    {
+        Scanner usrInput = new Scanner( System.in );
+
+        //Insert choice
+        String userchoice;
+        String[] hashes = {"SHA-256", "SHA-512", "MD4", "MD5"};
+        if(Objects.equals(encryptionChoice, "RSA"))
+        {
+            return "";
+        }
+        else {
+            System.out.println("Choose type of hash (none[default], SHA-256, SHA-512, MD4 or MD5): ");
+            userchoice = usrInput.nextLine();
+            if (!Arrays.asList(hashes).contains(userchoice)) {
+                userchoice = "";
+            }
+        }
         return userchoice;
     }
 }
