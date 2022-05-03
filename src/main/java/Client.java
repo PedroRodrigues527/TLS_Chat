3c4ed7d191cfbbc15557b5827cc397ede5591da7
PedroRodrigues527 <pedro.miguel.rodrigues00@gmail.com>
2022-05-03 10:11:14 +0100

    RSA algorithm added to client, to send, file encrypted

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,7 +1,14 @@
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.math.BigInteger;
 import java.net.Socket;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -10,18 +17,28 @@ public class Client {
     private final Socket client;
     private final ObjectInputStream in;
     private final ObjectOutputStream out;
-
+    private RSA rsa; //Final -> might not be initialized - ver!
+    private PublicKey receiverPublicRSAKey;
+    private BigInteger privateSharedDHKey;
     private final String userName, encryptionUser, hashUser;
     private final int keySizeUser;
 
-    public Client ( String host , int port , String userName, String encryptionUser, int keySizeUser, String hashUser ) throws IOException, ClassNotFoundException {
+    public Client ( String host , int port , String userName, String encryptionUser, int keySizeUser, String hashUser ) throws IOException, ClassNotFoundException, NoSuchAlgorithmException {
         client = new Socket( host , port );
         this.userName = userName;
         this.encryptionUser = encryptionUser;
         this.keySizeUser = keySizeUser;
         this.hashUser = hashUser;
-        out = new ObjectOutputStream( client.getOutputStream( ) );
-        in = new ObjectInputStream( client.getInputStream( ) );
+
+        if ( encryptionUser.equals( "RSA" ) ) {
+            rsa = new RSA( keySizeUser );
+            rsaKeyDistribution();
+        }
+
+        out = new ObjectOutputStream(client.getOutputStream());
+        in = new ObjectInputStream(client.getInputStream());
+
+
 
         //HELLO handshake
         out.writeObject( userName );
@@ -38,15 +55,92 @@ public class Client {
         System.out.println("Agora já pode enviar mensagens no chat.");
     }
 
+    public String getEncryptionUser() {
+        return encryptionUser;
+    }
+
+    public String getHashUser() {
+        return hashUser;
+    }
+
+    public int getKeySizeUser() {
+        return keySizeUser;
+    }
+
+
+    /*******************************RSA METHODS*********************************************************************************************************************************************/
+
+    private void rsaKeyDistribution () throws IOException, ClassNotFoundException {
+        // Sends the public key
+        sendPublicRSAKey( );
+        // Receive the public key of the sender
+        receivePublicRSAKey( );
+    }
+
+    private void sendPublicRSAKey () throws IOException {
+        out.writeObject( rsa.getPublicKey() );
+        out.flush( );
+    }
+
+    private void receivePublicRSAKey () throws IOException, ClassNotFoundException {
+        receiverPublicRSAKey = (PublicKey) in.readObject();
+    }
+
+    private BigInteger agreeOnSharedPrivateDHKey ( ObjectInputStream in , ObjectOutputStream out ) throws IOException, ClassNotFoundException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
+        // Generates a private key
+        BigInteger privateDHKey = DiffieHellman.generatePrivateKey( );
+        BigInteger publicDHKey = DiffieHellman.generatePublicKey( privateDHKey );
+        // Sends the public key to the server
+        sendPublicDHKey( publicDHKey );
+        // Waits for the server to send his public key
+        byte[] clientPublicDHKeyEncrypted = (byte[]) in.readObject( );
+        byte[] clientPublicDHKey = rsa.decrypt( clientPublicDHKeyEncrypted , receiverPublicRSAKey );
+        // Generates the common private key
+        //DiffieHellman diffieHellman = new DiffieHellman( getKeySizeUser( ) );
+        return DiffieHellman.computePrivateKey( new BigInteger( clientPublicDHKey ) , privateDHKey );
+    }
+
+    private void sendPublicDHKey ( BigInteger publicDHKey ) throws IOException {
+        out.writeObject( publicDHKey );
+        out.flush( );
+    }
+
+    private void sendRequest ( String message , ObjectOutputStream out ) throws IOException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, ClassNotFoundException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
+        // Computes the shared private key
+        privateSharedDHKey = agreeOnSharedPrivateDHKey( in , out );
+        byte[] messageInBytes = message.getBytes( );
+        out.writeObject( rsa.encrypt( messageInBytes ) );
+        out.flush( );
+    }
+
+
+/************************************************************************************************************************************************************************************************************************* */
+
+
     public void sendMessages () throws IOException {
         while ( client.isConnected( ) ) {
             Scanner usrInput = new Scanner( System.in );
             String message = usrInput.nextLine( );
             try {
+                if ( this.encryptionUser.equals("RSA") ){
+                    sendRequest( message , out );
+                }
                 out.writeObject( message );
             } catch ( IOException e ) {
                 closeConnection( );
                 break;
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (NoSuchPaddingException e) {
+                e.printStackTrace();
+            } catch (IllegalBlockSizeException e) {
+                e.printStackTrace();
+            } catch (BadPaddingException e) {
+                e.printStackTrace();
+            } catch (InvalidKeyException e) {
+                e.printStackTrace();
             }
         }
 
@@ -56,6 +150,9 @@ public class Client {
         new Thread( () -> {
             while ( client.isConnected( ) ) {
                 try {
+                    if ( this.encryptionUser.equals( "rsa" ) ) {
+
+                    }
                     ArrayList<Object> messageWithUserName = (ArrayList<Object>) in.readObject( );
                     String userName = (String) messageWithUserName.get( 0 );
                     String messageDecrypted = new String( (byte[]) messageWithUserName.get( 1 ) );
