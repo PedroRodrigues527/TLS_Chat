a4377f8a34b59b2dd92d00d5db5ebeef80e5a7cf
JoaoCupido <jcupido13@gmail.com>
2022-05-04 13:43:18 +0100

    Continuation of tests

--- src/test/java/MainClientTest.java
+++ src/test/java/MainClientTest.java
@@ -18,41 +18,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class MainClientTest {
 
     @Nested
-    @DisplayName("Server Requests tests")
-    class usernameChoiceTest
+    @DisplayName("Client choices' tests")
+    class ChoiceTests
     {
-
-        private final InputStream systemIn = System.in;
-        private final PrintStream systemOut = System.out;
-
-        private ByteArrayInputStream testIn;
-        private ByteArrayOutputStream testOut;
-
-        @BeforeEach
-        void setUp()
-        {
-            testOut = new ByteArrayOutputStream();
-            System.setOut(new PrintStream(testOut));
-        }
-
-        private void provideInput(String data) {
-            testIn = new ByteArrayInputStream(data.getBytes());
-            System.setIn(testIn);
-        }
-
-        private String getOutput() {
-            return testOut.toString();
-        }
-
-        @AfterEach
-        public void restoreSystemInputOutput() {
-            System.setIn(systemIn);
-            System.setOut(systemOut);
-        }
-
         @DisplayName ("Test username choice")
         @Test
-        public void testUsername() {
+        public void testUsernameChoice() {
             String userInput = String.format("User%s",
                     System.lineSeparator());
             ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
@@ -62,7 +33,7 @@ class MainClientTest {
             PrintStream printStream = new PrintStream(baos);
             System.setOut(printStream);
 
-            String userNameOutput = MainClient.usernameChoice(); // call the username method
+            String userNameOutput = MainClient.usernameChoice(); // call the username choice method
 
 
             String userInput2 = String.format("%stest%s",
@@ -75,12 +46,84 @@ class MainClientTest {
             PrintStream printStream2 = new PrintStream(baos2);
             System.setOut(printStream2);
 
-            String userNameOutput2 = MainClient.usernameChoice(); // call the main method
+            String userNameOutput2 = MainClient.usernameChoice();
 
             // checkout output
             assertAll(
                     () -> assertEquals("test",userNameOutput2),
                     () -> assertEquals("User",userNameOutput)
+            );
+        }
+
+        @DisplayName ("Test encryption choice")
+        @Test
+        public void testEncryptionChoice() {
+            String userInput = String.format("RSA%s",
+                    System.lineSeparator());
+            ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
+            System.setIn(bais);
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            PrintStream printStream = new PrintStream(baos);
+            System.setOut(printStream);
+
+            String encryptionOutput = MainClient.encryptionChoice(); // call the encryption choice method
+
+
+            String userInput2 = String.format("%st%sAES%s",
+                    System.lineSeparator(),
+                    System.lineSeparator(),
+                    System.lineSeparator());
+            ByteArrayInputStream bais2 = new ByteArrayInputStream(userInput2.getBytes());
+            System.setIn(bais2);
+
+            ByteArrayOutputStream baos2 = new ByteArrayOutputStream();
+            PrintStream printStream2 = new PrintStream(baos2);
+            System.setOut(printStream2);
+
+            String encryptionOutput2 = MainClient.encryptionChoice();
+
+            // checkout output
+            assertAll(
+                    () -> assertEquals("AES",encryptionOutput2),
+                    () -> assertEquals("RSA",encryptionOutput),
+                    () -> assertNotEquals("AES",encryptionOutput)
+            );
+        }
+
+        @DisplayName ("Test key size choice")
+        @Test
+        public void testKeySizeChoice() {
+            String userInput = String.format("128%s",
+                    System.lineSeparator());
+            ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
+            System.setIn(bais);
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            PrintStream printStream = new PrintStream(baos);
+            System.setOut(printStream);
+
+            int keySizeOutput = MainClient.keySizeChoice("AES"); // call the key size choice method
+
+
+            String userInput2 = String.format("%s1%s1024%s",
+                    System.lineSeparator(),
+                    System.lineSeparator(),
+                    System.lineSeparator());
+            ByteArrayInputStream bais2 = new ByteArrayInputStream(userInput2.getBytes());
+            System.setIn(bais2);
+
+            ByteArrayOutputStream baos2 = new ByteArrayOutputStream();
+            PrintStream printStream2 = new PrintStream(baos2);
+            System.setOut(printStream2);
+
+            int keySizeOutput2 = MainClient.keySizeChoice("RSA");
+
+            // checkout output
+            assertAll(
+                    () -> assertEquals(1024,keySizeOutput2),
+                    () -> assertEquals(128,keySizeOutput),
+                    () -> assertNotEquals(1,keySizeOutput2)
             );
         }
     }
